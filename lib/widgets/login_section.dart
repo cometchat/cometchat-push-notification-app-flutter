c17feea2e83c4a53a5f9b2ebadd1e5b192d66628
munikiran-cometchat <132883878+munikiran-cometchat@users.noreply.github.com>
2024-06-21 20:33:50 +0530

    Added Default Users

--- lib/widgets/login_section.dart
+++ lib/widgets/login_section.dart
@@ -1,81 +1,90 @@
+import 'package:cometchat_chat_uikit/cometchat_chat_uikit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_pn/services/api_services.dart';
 import 'package:flutter_pn/services/cometchat_service.dart';
 
-List<UserModel> userModelList = [
-  UserModel("superhero1", "SUPERHERO1", "assets/ironman_avatar.png"),
-  UserModel("superhero2", "SUPERHERO2", "assets/captainamerica_avatar.png"),
-  UserModel("superhero3", "SUPERHERO3", "assets/spiderman_avatar.png"),
-  UserModel("superhero5", "SUPERHERO5", "assets/cyclops_avatar.png"),
-];
-
-class UserModel {
-  String uid;
-  String username;
-  String imageURL;
-
-  UserModel(this.uid, this.username, this.imageURL);
-}
+import '../models/material_button_user_model.dart';
 
 class SampleUsersLoginSection extends StatelessWidget {
   final BuildContext thatContext;
 
   const SampleUsersLoginSection({
-    Key? key,
+    super.key,
     required this.thatContext,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      shrinkWrap: true,
-      crossAxisCount: 2,
-      crossAxisSpacing: 10.0,
-      mainAxisSpacing: 10.0,
-      childAspectRatio: 3.0,
-      children: userModelList
-          .map((model) => UserLoginButton(
-        model: model,
-        thatContext: thatContext,))
-          .toList(),
+    return FutureBuilder<List<MaterialButtonUserModel>>(
+      future: ApiServices.fetchUsers(),
+      builder: (BuildContext context,
+          AsyncSnapshot<List<MaterialButtonUserModel>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Show loader while fetching data
+          return Center(
+            child: Image.asset(
+              AssetConstants.spinner,
+              package: UIConstants.packageName,
+            ),
+          );
+        } else if (snapshot.hasError) {
+          // Handle error
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else {
+          // Show user selection buttons
+          return GridView.count(
+            shrinkWrap: true,
+            crossAxisCount: 2,
+            crossAxisSpacing: 10.0,
+            mainAxisSpacing: 10.0,
+            childAspectRatio: 3.0,
+            children: (snapshot.data ?? []).take(4).map((user) {
+              return userSelectionButton(user, context);
+            }).toList(),
+          );
+        }
+      },
     );
   }
-}
 
-class UserLoginButton extends StatelessWidget {
-  final UserModel model;
-  final BuildContext thatContext;
-
-  const UserLoginButton(
-      {Key? key,
-        required this.model,
-        required this.thatContext,})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
+  Widget userSelectionButton(
+    MaterialButtonUserModel model,
+    BuildContext context,
+  ) {
     return MaterialButton(
       color: Colors.black,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-      onPressed: () async{
-        CometChatService().login(
-            model.uid, context);
+      onPressed: () {
+        CometChatService().login(model.userId, context);
       },
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          CircleAvatar(
-            backgroundColor: Colors.white,
-            child: Image.asset(
-              model.imageURL,
-              height: 30,
-              width: 30,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.white,
+              child: model.imageURL.startsWith('http')
+                  ? Image.network(
+                      model.imageURL,
+                      height: 30,
+                      width: 30,
+                    )
+                  : Image.asset(
+                      model.imageURL,
+                      height: 30,
+                      width: 30,
+                    ),
             ),
-          ),
-          Text(
-            model.username,
-            style: const TextStyle(color: Colors.white, fontSize: 14.0),
-          )
-        ],
+            const SizedBox(width: 10.0),
+            Expanded(
+              child: Text(
+                model.username,
+                style: const TextStyle(color: Colors.white, fontSize: 14.0),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
